b8dbd75c686823bd49169590c288e39c881d9685
Kirill Trofimov <trofkm@gmail.com>
2021-05-19 12:24:39 +0300

    Send frome Notebook

--- QT_BMP.pro
+++ QT_BMP.pro
@@ -9,12 +9,14 @@ CONFIG += c++20
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    arc.cpp \
     main.cpp \
     mw.cpp \
     mypicture.cpp \
     pixeltable.cpp
 
 HEADERS += \
+    arc.h \
     mw.h \
     mypicture.h \
     pixeltable.h
--- mw.cpp
+++ mw.cpp
@@ -20,10 +20,11 @@ void MW::on_actionOpen_triggered()
     QString filename = QFileDialog::getOpenFileName(this,"Open the file");//Получаем имя файла
     ui->statusbar->showMessage(filename);
     Bitmap bmp(filename.toStdString());
-    ui->PictureView->setFixedSize(bmp.W()+15,bmp.H()+15);
+    //ui->PictureView->setFixedSize(bmp.W()+15,bmp.H()+15);//mb don't even want
     QImage image(filename);
     QGraphicsScene* scene = new QGraphicsScene;
     scene->addPixmap(QPixmap::fromImage(image));
     ui->PictureView->setScene(scene);
-    ui->PictureView->show();
+    ui->PictureView->show();//change from show to showFullScreen
+
 }
